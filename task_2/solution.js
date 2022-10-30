ccc97989672880c42d368e36c545eb435b3dc193
KharkovaOlga <ereyra@list.ru>
2022-10-30 22:46:09 +0300

    Update solution.js
    Co-Authored-By: OlgaKharkova <115497818+olgakharkova@users.noreply.github.com>

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,9 +4,11 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
    
     let shippingSum;   
-    if (productsSum == 0 ) (productsSum >= freeShippingMinSum),
+    if (productsSum = 0 )
     shippingSum = 0;
-    if (productsSum > 0 && productsSum < freeShippingMinSum)
+    if (productsSum >= freeShippingMinSum)
+    shippingSum = 0
+    if (productsSum > 0 & productsSum < freeShippingMinSum)
     shippingSum = shippingPrice;
     
 

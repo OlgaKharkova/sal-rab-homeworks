81d9b1fa96c3efce892505ec5f793c8962e8b3a3
KharkovaOlga <ereyra@list.ru>
2022-10-30 23:14:52 +0300

    Update solution.js
    Co-Authored-By: OlgaKharkova <115497818+olgakharkova@users.noreply.github.com>

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,12 +4,11 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
    
     let shippingSum;   
-    if (productsSum = 0 )
-    shippingSum = 0;
-    if (productsSum >= freeShippingMinSum)
-    shippingSum = 0
-    if (productsSum > 0 & productsSum < freeShippingMinSum)
-    shippingSum = shippingPrice;
+  if (productsSum == 0 || productsSum >= freeShippingMinSum);
+    shippingSum = 0; {
+} else if (productsSum > 0 && productsSum < freeShippingMinSum);
+    shippingSum = shippingPrice; {
+}
     
 
     // Задание №2.1. Рассчитать доставку

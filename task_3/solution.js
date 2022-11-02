b8f32f257eef1beec7711ce42dfba021c5fdc4fe
KharkovaOlga <ereyra@list.ru>
2022-11-02 20:07:13 +0300

    Update solution.js

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,14 +1,14 @@
 function renderCartItem(item) {
     const product = item;
-product = {id: 1, title:  'Пицца', count: 5, price: '500.00'};
-let itemCountText;
-itemCountText += count;
-itemCountText += ` x `;
-itemCountText += price;
-itemCountText += ` ₽ =`;
-let sum = price * count;
-itemCountText += sum;
-itemCountText += ` ₽`;
+product = {id: id, title:  title, count: count, price: price};
+let itemCountText = ;
+itemCountText = ${itemCountText}count;
+itemCountText = ${itemCountText}x;
+itemCountText = ${itemCountText}price;
+itemCountText = ${itemCountText}₽ =;
+let sum = count*price;
+itemCountText = ${itemCountText}sum;
+itemCountText = ${itemCountText}₽;
 
 
 

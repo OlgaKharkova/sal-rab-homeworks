b94d669b994da07aef8b6f0748a26fcd068d0743
KharkovaOlga <ereyra@list.ru>
2022-11-22 16:49:39 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {order: {}, goods: []};
 
     let countOfGoods = goods.length;
 
@@ -43,10 +43,10 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(test);
     }
 
-    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + кв " + address.flat ;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat;
     data.order.sum = sum;
 
-    data.client = name + ' ' + phone`;
+    data.client = name + ' ' + phone;
 
     let jsonData = JSON.stringify({data});
 
